7fa1e88185d5fa886ca45193b0095e36923ea94b
ozturksevgi <ozturksevgii@gmail.com>
2016-08-11 14:13:15 +0300

    Initial Commit

---
+++ CreditCardFormatter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreditCardFormatter
+//
+//  Created by Sevgi OZTURK on 11/08/16.
+//  Copyright © 2016 Sevgi OZTURK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
